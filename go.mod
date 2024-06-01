1c277314cbe1d927e2f22bbb4a983f97c78723ea
kittytree <151437342+kittytree@users.noreply.github.com>
2024-06-01 00:20:57 -0700

    intial commit
    learning go by doing bingo lol

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kittytree/go-play-bingo
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	player1, player2 := start()
+	fmt.Println(player1)
+	fmt.Println(player2)
+}
